6e27e8b3af670f474f89cd25d977c67ced578a5f
Palak Bhatt <bhattpalak234@gmail.com>
2026-01-31 18:40:17 +0530

    Added basic DSA practice file

---
+++ cpp_basic.cpp
@@ -0,0 +1,110 @@
+#include<iostream>
+#include <string>
+using namespace std;
+
+// int main(){
+
+    // int age;
+
+    // cout<<"Enter your age";
+    // cin>>age;
+
+    // if (age<18){
+    //     cout<<"not an adult";
+    // }
+    // else {cout<<"adult";}
+
+    // int marks;
+    // cout<<"Enter your marks";
+    // cin>>marks;
+
+    // if (marks<25){
+    //     cout<<"F";
+    // }
+    //  else if (marks<=44){
+    //     cout<<"E";
+    // }
+    //  else if (marks<=49){
+    //     cout<<"D";
+    // }
+    //  else if (marks<=59){
+    //     cout<<"C";
+    // }
+    //  else if (marks<=79){
+    //     cout<<"B";
+    // }
+    //  else if (marks<=100){
+    //     cout<<"A";
+    // }
+
+    // 
+    
+   
+        // int day;
+        // cin>>day;
+
+        // switch(day) {
+        //     case 1:
+        //       cout<< "Monday";
+        //       break;
+        //     case 2:
+        //       cout<< "Tuesday";
+        //       break;
+        //     case 3:
+        //       cout<< "Wednesday";
+        //       break;
+        //     case 4:
+        //       cout<< "Thursday";
+        //       break;
+        //     case 5:
+        //       cout<< "Friday";
+        //       break;
+        //     case 6:
+        //       cout<< "Saturday";
+        //       break;
+        //     case 7:
+        //       cout<< "Sunday";
+        //       break;
+        //     default:
+        //        cout<<"Invalid";
+
+        // }
+
+        
+//}
+
+
+// void printName(string name ){
+//     cout<<"hey "<< name <<'!'<<endl;
+// }
+
+// int main() {
+//     string name;
+//     cin>> name;
+//     printName(name);
+
+//     string name2;
+//     cin>>name2;
+//     printName(name2);
+//     return 0;
+    
+
+// }
+
+// int sum(int num1, int num2){
+
+//     int num3= num1+num2; //2.5+6
+//     return num3; //3. return 11
+// }
+
+// int main(){
+
+//     int num1, num2;
+//     cin>>num1>>num2;
+//     int res=sum(num1,num2); //1.funtionsum called. 4. res=11
+//     cout<<res; //cout res value 11
+//     return 0;
+
+// }
+
+ 
---
+++ patterns.cpp
@@ -0,0 +1,62 @@
+//Pattern1.
+// ****
+// ****
+// ****
+// ****  
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+
+//     for(int i=0;i<4;i++){
+//         for (int j=0;j<4;j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+    
+
+//     }
+
+// }
+
+// #include<iostream>
+// using namespace std;
+
+// void print1(int n){
+//     for (int i=0;i<n;i++){
+//         for(int j=0;j<n;j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+
+// }
+
+// int main(){
+//     int n;
+//     cout<<"Enter a no.";
+//     cin>>n;
+//     print1(n);
+// }
+
+// #include<iostream>
+// using namespace std;
+
+// void print1(int n){
+//     for (int i=0;i<n;i++){
+//         for(int j=0;j<=i;j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+
+// }
+
+// int main(){
+//     int n;
+//     cout<<"Enter a no.";
+//     cin>>n;
+//     print1(n);
+// }
+
