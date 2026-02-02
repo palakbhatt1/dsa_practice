49989a813ceb133b8d0ca600c45514ca8e6f10b9
Palak Bhatt <bhattpalak234@gmail.com>
2026-02-02 17:56:38 +0530

    added more patterns

--- patterns.cpp
+++ patterns.cpp
@@ -20,12 +20,38 @@
 
 // }
 
+
 // #include<iostream>
 // using namespace std;
 
 // void print1(int n){
 //     for (int i=0;i<n;i++){
 //         for(int j=0;j<n;j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+
+// }
+
+
+// int main(){
+//     int n;
+//     cout<<"Enter a no.";
+//     cin>>n;
+//     print1(n);
+// }
+
+// *
+// **
+// ***
+
+// #include<iostream>
+// using namespace std;
+
+// void print1(int n){
+//     for (int i=0;i<n;i++){
+//         for(int j=0;j<=i;j++){
 //             cout<<"*";
 //         }
 //         cout<<endl;
@@ -40,13 +66,93 @@
 //     print1(n);
 // }
 
+// 1
+// 12
+// 123
+// 1234
+
 // #include<iostream>
 // using namespace std;
 
 // void print1(int n){
-//     for (int i=0;i<n;i++){
-//         for(int j=0;j<=i;j++){
+//     for (int i=1;i<=n;i++){
+//         for(int j=1;j<=i;j++){
+//             cout<<j;
+//         }
+//         cout<<endl;
+//     }
+
+// }
+
+// int main(){
+//     int n;
+//     cout<<"Enter a no.";
+//     cin>>n;
+//     print1(n);
+// }
+
+// 1
+// 22
+// 333
+// 4444
+
+// #include<iostream>
+// using namespace std;
+
+// void print1(int n){
+//     for (int i=1;i<=n;i++){
+//         for(int j=1;j<=i;j++){
+//             cout<<i;
+//         }
+//         cout<<endl;
+//     }
+
+// }
+
+// int main(){
+//     int n;
+//     cout<<"Enter a no.";
+//     cin>>n;
+//     print1(n);
+// }
+
+// ****
+// ***
+// **
+// *
+
+// #include<iostream>
+// using namespace std;
+
+// void print1(int n){
+//     for (int i=1;i<=n;i++){
+//         for(int j=0;j<n-i+1;j++){
 //             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+
+// }
+
+// int main(){
+//     int n;
+//     cout<<"Enter a no.";
+//     cin>>n;
+//     print1(n);
+// }
+
+// 12345
+// 1234
+// 123
+// 12
+// 1
+// #include<iostream>
+// using namespace std;
+
+// void print1(int n){
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=n-i+1;j++){
+//             cout<<j;
 //         }
 //         cout<<endl;
 //     }
