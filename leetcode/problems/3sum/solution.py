589ee442596f00abb38f3800e688cfcc16905e65
palakbhatt1 <150253651+palakbhatt1@users.noreply.github.com>
2026-06-27 20:07:48 +0000

    Sync LeetCode submission - 3Sum (python3)

--- leetcode/problems/3sum/solution.py
+++ leetcode/problems/3sum/solution.py
@@ -1,33 +1,41 @@
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
         nums.sort()
-        res = []
+        res =[]
 
-        for i in range(len(nums) - 2):
-      
-            if i > 0 and nums[i] == nums[i - 1]:
+        for i in range (len(nums)-2):
+            if i>0 and nums[i] == nums[i-1]:
                 continue
 
-            left, right = i + 1, len(nums) - 1
+            target = - nums[i]
+
+            left = i+1
+            right = len(nums)-1
 
             while left < right:
-                total = nums[i] + nums[left] + nums[right]
-
-                if total < 0:
-                    left += 1
-                elif total > 0:
-                    right -= 1
-                else:
+                 
+                if nums[left] + nums[right] == target:
                     res.append([nums[i], nums[left], nums[right]])
 
-                    left += 1
-                    right -= 1
+                    left+=1
+                    right-=1
 
-               
-                    while left < right and nums[left] == nums[left - 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right + 1]:
-                        right -= 1
+                    while left < right and nums[left] == nums[left-1]:
+                        left +=1
+
+                    while left <right  and nums[right] == nums[right+1]:
+                        right -=1
+                        
+                elif nums[left]+ nums[right] < target:
+                    left += 1
+
+                else:
+                    right -=1
 
         return res
-    
+
+                    
+                        
+
+
+
