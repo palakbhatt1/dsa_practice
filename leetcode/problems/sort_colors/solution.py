3263964b2f024ec16a0d3340826e4a52d3043fab
palakbhatt1 <150253651+palakbhatt1@users.noreply.github.com>
2026-06-28 18:54:20 +0000

    Sync LeetCode submission - Sort Colors (python3)

--- leetcode/problems/sort_colors/solution.py
+++ leetcode/problems/sort_colors/solution.py
@@ -1,5 +1,46 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        nums.sort()
-        return nums
+        
+        zero = 0
+        one = 0
+        two = 0
+
+        for i in range (len(nums)):
+            if nums[i] == 0:
+                zero += 1
+
+            elif nums[i]==1:
+                one += 1
+
+            else: 
+                two += 1
+
+        i = 0
+
+        while zero > 0:
+            nums[i] = 0
+            i += 1
+            zero -= 1
+
+        while one > 0:
+            nums[i] = 1
+            i += 1
+            one -= 1
+
+        while two > 0:
+            nums[i] = 2
+            i += 1
+            two -= 1
+
+        
+
+
+    
+
+
+
+
+        
+
+
         
