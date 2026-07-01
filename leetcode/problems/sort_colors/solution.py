b2888c9830b9ba9eacc1d9fe353213b26b5e7c93
palakbhatt1 <150253651+palakbhatt1@users.noreply.github.com>
2026-07-01 17:02:29 +0000

    Sync LeetCode submission - Sort Colors (python3)

--- leetcode/problems/sort_colors/solution.py
+++ leetcode/problems/sort_colors/solution.py
@@ -1,36 +1,57 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        
-        zero = 0
-        one = 0
-        two = 0
+        #two pass approach
+        # zero = 0
+        # one = 0
+        # two = 0
 
-        for i in range (len(nums)):
-            if nums[i] == 0:
-                zero += 1
+        # for i in range (len(nums)):
+        #     if nums[i] == 0:
+        #         zero += 1
 
-            elif nums[i]==1:
-                one += 1
+        #     elif nums[i]==1:
+        #         one += 1
 
-            else: 
-                two += 1
+        #     else: 
+        #         two += 1
 
-        i = 0
+        # i = 0
 
-        while zero > 0:
-            nums[i] = 0
-            i += 1
-            zero -= 1
+        # while zero > 0:
+        #     nums[i] = 0
+        #     i += 1
+        #     zero -= 1
 
-        while one > 0:
-            nums[i] = 1
-            i += 1
-            one -= 1
+        # while one > 0:
+        #     nums[i] = 1
+        #     i += 1
+        #     one -= 1
 
-        while two > 0:
-            nums[i] = 2
-            i += 1
-            two -= 1
+        # while two > 0:
+        #     nums[i] = 2
+        #     i += 1
+        #     two -= 1'
+
+        #single pass approach
+
+        left = 0
+        mid = 0
+        right = len(nums) - 1
+
+        while mid <=right :
+            
+            if nums [mid] == 0:
+                nums[left], nums[mid] = nums [mid], nums[left]
+                left += 1
+                mid +=1
+
+            elif nums[mid] == 1:
+                mid += 1
+
+            else:
+                nums[right], nums[mid] = nums [mid], nums[right]
+                right -= 1
+                
 
         
 
