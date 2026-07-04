02c8bbca5f3292f79a34ed06e45513b2057580e6
Palak Bhatt <bhattpalak234@gmail.com>
2026-07-04 19:28:17 +0530

    Sync submission - Max Sum Subarray of size K (python3)
    .

---
+++ leetcode/problems/max_sum_subarray_of_size_k/solution.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxSubarraySum(self, arr, k):
+        
+        low = 0
+        high = k-1
+        total = 0
+        res = 0
+        
+        for i in range (low, high +1):
+                total += arr[i]
+                
+        res = total
+                
+        while high < len(arr)- 1:
+            
+            low +=1
+            high +=1
+            
+            total = total - arr[low -1] + arr[high]
+            
+            res =  max(res, total)
+        
+        return res
+            
+        
