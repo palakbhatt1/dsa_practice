8dbbd64bf3c30bf2dc2f11f0b6640ecba4c5cdd2
palakbhatt1 <150253651+palakbhatt1@users.noreply.github.com>
2026-08-17 21:02:44 +0000

    Sync LeetCode submission - Longest Harmonious Subsequence (python3)

---
+++ leetcode/problems/longest_harmonious_subsequence/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:
+       
+        count = Counter(nums)
+        
+        ans = 0
+        
+        for x in count:
+            if x + 1 in count:
+                ans = max(ans, count[x] + count[x + 1])
+        
+        return ans
