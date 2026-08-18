d05ebefa29245ae245d95d4a916496a46615d6ab
palakbhatt1 <150253651+palakbhatt1@users.noreply.github.com>
2026-08-18 20:01:11 +0000

    Sync LeetCode submission - Confirmation Rate (mysql)

---
+++ leetcode/problems/confirmation_rate/solution.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select s.user_id, round(
+        coalesce(avg(c.action = 'confirmed'), 0),2) as confirmation_rate
+from signups s
+left join confirmations c
+on s.user_id = c.user_id
+group by s.user_id
