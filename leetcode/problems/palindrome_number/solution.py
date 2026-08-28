72239965d6dd0d2a2d3b4533228ea85ae8a6a5eb
palakbhatt1 <150253651+palakbhatt1@users.noreply.github.com>
2026-08-28 19:04:17 +0000

    Sync LeetCode submission - Palindrome Number (python3)

--- leetcode/problems/palindrome_number/solution.py
+++ leetcode/problems/palindrome_number/solution.py
@@ -1,8 +1,18 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        s = str(x)
 
-        if s == s[::-1]:
+        org = x
+        rev = 0
+
+        while x > 0:
+
+            r = x % 10
+            rev = rev*10 + r
+            x = x//10
+        
+        if org == rev:
             return True
         else:
             return False
+
+        
